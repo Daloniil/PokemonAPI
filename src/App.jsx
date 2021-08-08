a37b9b5ddff9b63de3dc9997cbd44f50b63c02b3
Daloniil <62095873+Daloniil@users.noreply.github.com>
2021-08-09 01:22:04 +0300

    Dissable

--- src/App.jsx
+++ src/App.jsx
@@ -68,43 +68,40 @@ export const App = () => {
 
 
     return (
-        <>
-            <form onSubmit={handleSubmit(onSubmit)} className={s.form}>
-                <select className={s.custom_select} {...register("selectPokemon")}>
-                    <option value="noselect">No select</option>
-                    <option value="normal">Normal</option>
-                    <option value="fighting">Fighting</option>
-                    <option value="flying">Flying</option>
-                    <option value="poison">Poison</option>
-                    <option value="ground">Ground</option>
-                    <option value="rock">Rock</option>
-                    <option value="bug">Bug</option>
-                    <option value="ghost">Ghost</option>
-                    <option value="steel">Steel</option>
-                    <option value="fire">Fire</option>
-                    <option value="water">Water</option>
-                    <option value="grass">Grass</option>
-                    <option value="electric">Electric</option>
-                    <option value="psychic">Psychic</option>
-                    <option value="ice">Ice</option>
-                    <option value="dragon">Dragon</option>
-                    <option value="dark">Dark</option>
-                    <option value="fairy">Fairy</option>
-                    <option value="shadow">Shadow</option>
-                </select>
-                <button type="submit">Search</button>
-            </form>
-            <div>
-                {loading ? <h1 style={{textAlign: 'center'}}>Loading...</h1> : (
-                    <>
-                        <div>
-                            <Card pokemonData={pokemonData} LoadMore={LoadMore} values={values}/>
-                        </div>
-                    </>
-                )}
-            </div>
-
-        </>
+        <div>
+            {loading ? <h1 style={{textAlign: 'center'}}>Loading...</h1> : (
+                <>
+                    <form onSubmit={handleSubmit(onSubmit)} className={s.form}>
+                        <select className={s.custom_select} {...register("selectPokemon")}>
+                            <option value="noselect">No select</option>
+                            <option value="normal">Normal</option>
+                            <option value="fighting">Fighting</option>
+                            <option value="flying">Flying</option>
+                            <option value="poison">Poison</option>
+                            <option value="ground">Ground</option>
+                            <option value="rock">Rock</option>
+                            <option value="bug">Bug</option>
+                            <option value="ghost">Ghost</option>
+                            <option value="steel">Steel</option>
+                            <option value="fire">Fire</option>
+                            <option value="water">Water</option>
+                            <option value="grass">Grass</option>
+                            <option value="electric">Electric</option>
+                            <option value="psychic">Psychic</option>
+                            <option value="ice">Ice</option>
+                            <option value="dragon">Dragon</option>
+                            <option value="dark">Dark</option>
+                            <option value="fairy">Fairy</option>
+                            <option value="shadow">Shadow</option>
+                        </select>
+                        <button type="submit">Search</button>
+                    </form>
+                    <div>
+                        <Card pokemonData={pokemonData} LoadMore={LoadMore} values={values}/>
+                    </div>
+                </>
+            )}
+        </div>
     );
 
 
